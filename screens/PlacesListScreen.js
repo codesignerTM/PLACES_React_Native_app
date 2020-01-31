88a0a2f181fbafac3d7d05ea1213d4c61dae3885
Tamás <codesigner.hu@gmail.com>
2020-01-31 22:42:17 +0100

    screen improvements

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButtons";
+import HeaderButton from "../components/HeaderButtons";
 
 const PlacesListScreen = props => {
   return (
@@ -7,6 +10,23 @@ const PlacesListScreen = props => {
       <Text>PlacesListScreen</Text>
     </View>
   );
+};
+
+PlacesListScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "All places",
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Add Place"
+          iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+          onPress={() => {
+            navData.navigation.navigate("NewPlace");
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
